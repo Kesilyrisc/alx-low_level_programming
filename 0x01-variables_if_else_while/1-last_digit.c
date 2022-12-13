220361652330dd9f6efd42396915623b6893adec
kesilyrisc <kesilyrisc@gmail.com>
2022-12-12 22:03:44 -0800

    Update

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * return 0
+ */
+int main(void)
+{
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
+	int m;
+
+	m = n % 10
+	if (m > 5)
+		return (0)
+}
